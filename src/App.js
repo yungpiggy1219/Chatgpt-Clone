ffdb73e5f2b3ba30fa097b73a1627055befe5a64
yungpiggy1219 <43735672+yungpiggy1219@users.noreply.github.com>
2024-07-14 10:51:22 -0500

    Basic Function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,88 @@
+// src/App.js
+
 import "./App.css";
+import { useState } from "react";
 import Header from "./Components/Header.jsx";
-import ChatForm from "./Components/ChatForm.jsx";
-import AnswerSec from "./Components/Answers.jsx";
+import FormSection from "./Components/FormSection.jsx";
+import AnswerSection from "./Components/AnswerSection.jsx";
 import OpenAI from "openai";
 
-function App() {
+const App = () => {
   const openai = new OpenAI({
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
   });
 
+  const [storedValues, setStoredValues] = useState([]);
+
+  /*   async function main() {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: "Who won the world series in 2020?" },
+        {
+          role: "assistant",
+          content: "The Los Angeles Dodgers won the World Series in 2020.",
+        },
+        { role: "user", content: "Where was it played?" },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion.choices[0]);
+  } */
+
+  const generateResponse = async (newQuestion, setNewQuestion) => {
+    let options = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content: newQuestion,
+        },
+      ],
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["/"],
+    };
+
+    let completeOptions = {
+      ...options,
+      /*      prompt: newQuestion, */
+    };
+
+    const chatCompletion = await openai.chat.completions.create(
+      completeOptions
+    );
+    console.log(chatCompletion.choices[0].message);
+    console.log("text: " + chatCompletion.choices[0].text);
+
+    if (chatCompletion.choices) {
+      setStoredValues([
+        {
+          question: newQuestion,
+          answer: chatCompletion.choices[0].message.content,
+        },
+        ...storedValues,
+      ]);
+      setNewQuestion("");
+    }
+  };
+
   return (
-    <div className="App">
+    <div>
       <Header />
-      <ChatForm />
-      <AnswerSec />
+      <FormSection generateResponse={generateResponse} />
+      <AnswerSection storedValues={storedValues} />
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="headerSubDiv">
-        <h1 className="headerText">ChatGPT CLONE</h1>
+        <h1 className="headerText">ChatGPT CLONE 🤖</h1>
         <br />
         <p className="headerSubText">
           I am ChatGPT, an AI language model developed by OpenAI. I'm here to
