d66f20608d6695d97717dda116094b166d7e3033
yungpiggy1219 <43735672+yungpiggy1219@users.noreply.github.com>
2024-07-14 12:56:49 -0500

    Clean

--- src/App.js
+++ src/App.js
@@ -15,36 +15,17 @@ const App = () => {
 
   const [storedValues, setStoredValues] = useState([]);
 
-  /*   async function main() {
-    const completion = await openai.chat.completions.create({
-      messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        { role: "user", content: "Who won the world series in 2020?" },
-        {
-          role: "assistant",
-          content: "The Los Angeles Dodgers won the World Series in 2020.",
-        },
-        { role: "user", content: "Where was it played?" },
-      ],
-      model: "gpt-3.5-turbo",
-    });
-
-    console.log(completion.choices[0]);
-  } */
-
   const generateResponse = async (newQuestion, setNewQuestion) => {
     let options = {
       model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: "You are a helpful assistant.",
-        },
-        {
-          role: "user",
-          content: newQuestion,
-        },
-      ],
+      messages: [      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: newQuestion,
+      },],
       temperature: 0,
       max_tokens: 100,
       top_p: 1,
@@ -53,16 +34,7 @@ const App = () => {
       stop: ["/"],
     };
 
-    let completeOptions = {
-      ...options,
-      /*      prompt: newQuestion, */
-    };
-
-    const chatCompletion = await openai.chat.completions.create(
-      completeOptions
-    );
-    console.log(chatCompletion.choices[0].message);
-    console.log("text: " + chatCompletion.choices[0].text);
+    const chatCompletion = await openai.chat.completions.create(options);
 
     if (chatCompletion.choices) {
       setStoredValues([
